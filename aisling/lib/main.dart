cd1eb2f5dbfea0620e267af78e3540218fe9dcc6
tknott95 <tknott95@hotmail.com>
2022-09-29 13:00:30 -0600

    modularizing - graphics/fans widget

--- aisling/lib/main.dart
+++ aisling/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:sleek_circular_slider/sleek_circular_slider.dart';
+
+
+import 'package:aisling/widgets/graphics/graphics.widget.dart';
 
 import 'dart:async';
 
@@ -7,24 +9,7 @@ import 'dart:async';
 /* finish apis */
 /* create an actual archi here */
 /* prob just tabs for now */
-import 'package:http/http.dart' as http;
 
-Future<void> setFansFetch(int _gpuIndex, int _val) async {
-  /* I use GET instead of POST with auth */
-  // Map<String, String> myHeaders = Map<String, String>();
-  // myHeaders ['alice'] = 'top_secret_key<kdkljsdljkdsjklkljsdkjlsdkljsdjklsdjklkjlsdjksdkjlsdkjlklsjdkjlsdljk>';
-
-   print("\n SETTING FANS");
-    Map<String, String> requestHeaders = {
-      'Content-type': 'application/json',
-      'alice': 'top_secret_key<kdkljsdljkdsjklkljsdkjlsdkljsdjklsdjklkjlsdjksdkjlsdkjlklsjdkjlsdljk>'
-  };
-
-  final backendURL = Uri.parse('http://192.168.0.8:8080/api/setFans/$_gpuIndex/$_val');
-
- http.Response response = await http.get(backendURL, headers: requestHeaders);
- print(response.body);
-}
 
 void main() {
   runApp(const MyApp());
@@ -138,67 +123,3 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-class SliderWidgetGraphics extends StatefulWidget {
-  final gpuIndex;
-  const SliderWidgetGraphics({Key? key, @required this.gpuIndex}) : super(key: key);
-
-  @override
-  State<SliderWidgetGraphics> createState() => _SliderWidgetStateGraphics();
-}
-
-class _SliderWidgetStateGraphics extends State<SliderWidgetGraphics> {
-  var gpuIndex;
-  @override
-  void initState() {
-    gpuIndex = widget.gpuIndex;
-
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return  Column(
-              children: [
-                Text(
-                  'GPU_$gpuIndex GRAPHICS',
-                  style: Theme.of(context).textTheme.headline6,
-                  ),
-                SleekCircularSlider(
-                  min: 0,
-                  max: 80,
-                  initialValue: 5,
-                  innerWidget: (sliderValue) => Center(
-                    child: Text(
-                        "+"+sliderValue.toStringAsFixed(0),
-                        style: Theme.of(context).textTheme.bodyText1,
-                      )
-                    ),
-                  appearance: CircularSliderAppearance(
-                    size: 85,
-                    customWidths: CustomSliderWidths(
-                      //handlerSize: 20,
-                      trackWidth: 1,
-                      //shadowWidth: 0,
-                      progressBarWidth: 11,
-                    ),
-                    customColors: CustomSliderColors(
-                      dotColor: const Color.fromARGB(175, 255, 255, 255)/*Theme.of(context).primaryColor*/,
-                      trackColor: const Color.fromARGB(172, 0, 0, 0),
-                      progressBarColors: const [
-                      Color.fromARGB(255, 240, 40, 13),
-                      Color.fromARGB(255, 238, 169, 20),
-                      Color.fromARGB(255, 8, 63, 3),
-                      Color.fromARGB(255, 50, 192, 45),
-                      ],
-                     shadowColor: const Color.fromARGB(255, 0, 0, 0),
-                    ),
-                  ),
-                  onChange: (double value) {
-                    // nvidia_set_graphics_clock(gpuIndex, value.round());
-                    setFansFetch(gpuIndex, value.round());
-                  }
-                ),
-              ],
-            );
-  }
-}
