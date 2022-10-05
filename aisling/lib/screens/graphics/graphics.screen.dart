a07007aaa504d91d86a4bc61c35f632dff58a04d
tknott95 <tknott95@hotmail.com>
2022-10-05 12:14:04 -0600

    mem clocking screen

--- aisling/lib/screens/graphics/graphics.screen.dart
+++ aisling/lib/screens/graphics/graphics.screen.dart
@@ -2,8 +2,42 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:aisling/widgets/graphics/graphics.widget.dart';
 
+
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+Future<int> fetchMemClockData(int _gpuIndex) async {
+  /* I use GET instead of POST with auth */
+  // Map<String, String> myHeaders = Map<String, String>();
+  // myHeaders ['alice'] = 'top_secret_key<kdkljsdljkdsjklkljsdkjlsdkljsdjklsdjklkjlsdjksdkjlsdkjlklsjdkjlsdljk>';
+
+  print("\n fetchFansData($_gpuIndex)");
+  Map<String, String> requestHeaders = {
+    'Content-type': 'application/json',
+    'alice': 'top_secret_key<kdkljsdljkdsjklkljsdkjlsdkljsdjklsdjklkjlsdjksdkjlsdkjlklsjdkjlsdljk>'
+  };
+
+  final backendURL = Uri.parse('http://192.168.0.8:8080/api/get/memoryClock');
+
+  http.Response response = await http.get(backendURL, headers: requestHeaders);
+ 
+  /* @TODO - build models to consume for code instantiation */
+  print(response.body);
+  print(json.decode(response.body));
+  print(json.decode(response.body)['gpuTemp']);
+  print(json.decode(response.body)['gpuTemp'][_gpuIndex]['gpuVal']);
+  // print(json.decode(response.body)['fans'][1]['gpuVal']);
+
+  int _powerDrawVal = int.parse(json.decode(response.body)['memoryClock'][_gpuIndex]['gpuVal']);
+
+  // return "$fanSpeed";
+  return _powerDrawVal;
+}
+
 class GraphicsScreen extends StatelessWidget {
   const GraphicsScreen({super.key});
+
+
 
   @override
   Widget build(BuildContext context) {
--- aisling/lib/screens/memory/memory_clocking.screen.dart
+++ aisling/lib/screens/memory/memory_clocking.screen.dart
@@ -2,6 +2,37 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:aisling/widgets/memory/memory.widget.dart';
 
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+Future<int> fetchMemClockData(int _gpuIndex) async {
+  /* I use GET instead of POST with auth */
+  // Map<String, String> myHeaders = Map<String, String>();
+  // myHeaders ['alice'] = 'top_secret_key<kdkljsdljkdsjklkljsdkjlsdkljsdjklsdjklkjlsdjksdkjlsdkjlklsjdkjlsdljk>';
+
+  print("\n fetchFansData($_gpuIndex)");
+  Map<String, String> requestHeaders = {
+    'Content-type': 'application/json',
+    'alice': 'top_secret_key<kdkljsdljkdsjklkljsdkjlsdkljsdjklsdjklkjlsdjksdkjlsdkjlklsjdkjlsdljk>'
+  };
+
+  final backendURL = Uri.parse('http://192.168.0.8:8080/api/get/memoryClock');
+
+  http.Response response = await http.get(backendURL, headers: requestHeaders);
+ 
+  /* @TODO - build models to consume for code instantiation */
+  print(response.body);
+  print(json.decode(response.body));
+  print(json.decode(response.body)['gpuTemp']);
+  print(json.decode(response.body)['gpuTemp'][_gpuIndex]['gpuVal']);
+  // print(json.decode(response.body)['fans'][1]['gpuVal']);
+
+  int _powerDrawVal = int.parse(json.decode(response.body)['memoryClock'][_gpuIndex]['gpuVal']);
+
+  // return "$fanSpeed";
+  return _powerDrawVal;
+}
+
 class MemoryClockingScreen extends StatelessWidget {
   const MemoryClockingScreen({super.key});
 
@@ -12,6 +43,55 @@ class MemoryClockingScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
+            Text(
+              "Memory Clock",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                height: 1, fontSize: 30 
+              )
+            ),
+            /* pull these future builders into own widget */
+             Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              // crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+         
+                 FutureBuilder(
+                  future: fetchMemClockData(0), 
+                  /*fetchFansData(0)*/
+                  builder: (BuildContext context, AsyncSnapshot<int> text) {
+                    if (text.connectionState == ConnectionState.waiting) {
+                      return new Text('loading data..');
+                      // new Text(text.data!);
+                    } else return new Text(
+                      "+"+text.data.toString(),
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          height: 3, fontSize: 30 
+                        )
+                      );
+                  }
+                ),
+                SizedBox(width: 50),
+                FutureBuilder(
+                  future: fetchMemClockData(1), 
+                  /*fetchFansData(0)*/
+                  builder: (BuildContext context, AsyncSnapshot<int> text) {
+                    if (text.connectionState == ConnectionState.waiting) {
+                      return new Text('loading data..');
+                      // new Text(text.data!);
+                    } else return new Text(
+                      "+"+text.data.toString(),
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          height: 3, fontSize: 30 
+                        )
+                      );
+                  }
+                ),
+               ]
+             ),
+
             SliderWidgetMemory(gpuIndex: 0),
             SizedBox(height: 50),
             SliderWidgetMemory(gpuIndex: 1),
