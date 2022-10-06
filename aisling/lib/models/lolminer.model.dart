8661b2e6da866e7c01741530adf0fe1c4f33d649
tknott95 <tknott95@hotmail.com>
2022-10-05 22:14:19 -0600

    mining model

---
+++ aisling/lib/models/lolminer.model.dart
@@ -0,0 +1,209 @@
+import 'dart:convert';
+
+Lolminer lolminerFromJson(String str) => Lolminer.fromJson(json.decode(str));
+
+String lolminerToJson(Lolminer data) => json.encode(data.toJson());
+
+class Lolminer {
+    Lolminer({
+        required this.software,
+        required this.session,
+        required this.numWorkers,
+        this.workers,
+        this.numAlgorithms,
+        this.algorithms,
+    });
+
+    final String software;
+    final Session? session;
+    final int numWorkers;
+    final List<Worker>? workers;
+    final int? numAlgorithms;
+    final List<Algorithm>? algorithms;
+
+    factory Lolminer.fromJson(Map<String, dynamic> json) => Lolminer(
+        software: json["Software"],
+        session: json["Session"] == null ? null : Session.fromJson(json["Session"]),
+        numWorkers: json["Num_Workers"],
+        workers: json["Workers"] == null ? null : List<Worker>.from(json["Workers"].map((x) => Worker.fromJson(x))),
+        numAlgorithms: json["Num_Algorithms"],
+        algorithms: json["Algorithms"] == null ? null : List<Algorithm>.from(json["Algorithms"].map((x) => Algorithm.fromJson(x))),
+    );
+
+    Map<String, dynamic> toJson() => {
+        "Software": software,
+        "Session": session == null ? null : session!.toJson(),
+        "Num_Workers": numWorkers,
+        "Workers": workers == null ? null : List<dynamic>.from(workers!.map((x) => x.toJson())),
+        // "Num_Algorithms": numAlgorithms == null ? null : numAlgorithms,
+        // "Algorithms": algorithms == null ? null : List<dynamic>.from(algorithms!.map((x) => x.toJson())),
+    };
+}
+
+class Algorithm {
+    Algorithm({
+        required this.algorithm,
+        required this.algorithmAppendix,
+        required this.pool,
+        required this.user,
+        required this.worker,
+        required this.performanceUnit,
+        required this.performanceFactor,
+        required this.totalPerformance,
+        required this.totalAccepted,
+        required this.totalRejected,
+        required this.totalStales,
+        required this.totalErrors,
+        required this.workerPerformance,
+        required this.workerAccepted,
+        required this.workerRejected,
+        required this.workerStales,
+        required this.workerErrors,
+    });
+
+    final String algorithm;
+    final String algorithmAppendix;
+    final String pool;
+    final String user;
+    final String worker;
+    final String performanceUnit;
+    final int performanceFactor;
+    final double totalPerformance;
+    final int totalAccepted;
+    final int totalRejected;
+    final int totalStales;
+    final int totalErrors;
+    final List<double>? workerPerformance;
+    final List<int>? workerAccepted;
+    final List<int>? workerRejected;
+    final List<int>? workerStales;
+    final List<int>? workerErrors;
+
+    factory Algorithm.fromJson(Map<String, dynamic> json) => Algorithm(
+        algorithm: json["Algorithm"],
+        algorithmAppendix: json["Algorithm_Appendix"],
+        pool: json["Pool"],
+        user: json["User"],
+        worker: json["Worker"],
+        performanceUnit: json["Performance_Unit"],
+        performanceFactor: json["Performance_Factor"],
+        totalPerformance: json["Total_Performance"] == null ? null : json["Total_Performance"].toDouble(),
+        totalAccepted: json["Total_Accepted"],
+        totalRejected: json["Total_Rejected"],
+        totalStales: json["Total_Stales"],
+        totalErrors: json["Total_Errors"],
+        workerPerformance: json["Worker_Performance"] == null ? null : List<double>.from(json["Worker_Performance"].map((x) => x.toDouble())),
+        workerAccepted: json["Worker_Accepted"] == null ? null : List<int>.from(json["Worker_Accepted"].map((x) => x)),
+        workerRejected: json["Worker_Rejected"] == null ? null : List<int>.from(json["Worker_Rejected"].map((x) => x)),
+        workerStales: json["Worker_Stales"] == null ? null : List<int>.from(json["Worker_Stales"].map((x) => x)),
+        workerErrors: json["Worker_Errors"] == null ? null : List<int>.from(json["Worker_Errors"].map((x) => x)),
+    );
+
+    Map<String, dynamic> toJson() => {
+        "Algorithm": algorithm,
+        "Algorithm_Appendix": algorithmAppendix,
+        "Pool": pool,
+        "User": user,
+        "Worker": worker,
+        "Performance_Unit": performanceUnit,
+        "Performance_Factor": performanceFactor,
+        "Total_Performance": totalPerformance,
+        "Total_Accepted": totalAccepted,
+        "Total_Rejected": totalRejected,
+        "Total_Stales": totalStales,
+        "Total_Errors": totalErrors,
+        "Worker_Performance": workerPerformance == null ? null : List<dynamic>.from(workerPerformance!.map((x) => x)),
+        "Worker_Accepted": workerAccepted == null ? null : List<dynamic>.from(workerAccepted!.map((x) => x)),
+        "Worker_Rejected": workerRejected == null ? null : List<dynamic>.from(workerRejected!.map((x) => x)),
+        "Worker_Stales": workerStales == null ? null : List<dynamic>.from(workerStales!.map((x) => x)),
+        "Worker_Errors": workerErrors == null ? null : List<dynamic>.from(workerErrors!.map((x) => x)),
+    };
+}
+
+class Session {
+    Session({
+        required this.startup,
+        required this.startupString,
+        required this.uptime,
+        required this.lastUpdate,
+    });
+
+    final int startup;
+    final String startupString;
+    final int uptime;
+    final int lastUpdate;
+
+    factory Session.fromJson(Map<String, dynamic> json) => Session(
+        startup: json["Startup"],
+        startupString: json["Startup_String"],
+        uptime: json["Uptime"],
+        lastUpdate: json["Last_Update"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "Startup": startup,
+        "Startup_String": startupString,
+        "Uptime": uptime,
+        "Last_Update": lastUpdate,
+    };
+}
+
+class Worker {
+    Worker({
+        this.index,
+        this.name,
+        this.power,
+        this.cclk,
+        this.mclk,
+        this.coreTemp,
+        this.jucTemp,
+        this.memTemp,
+        this.fanSpeed,
+        this.lhrUnlockPct,
+        this.dualFactor,
+        this.pcieAddress,
+    });
+
+    final int? index;
+    final String? name;
+    final double? power;
+    final int? cclk;
+    final int? mclk;
+    final int? coreTemp;
+    final int? jucTemp;
+    final int? memTemp;
+    final int? fanSpeed;
+    final int? lhrUnlockPct;
+    final int? dualFactor;
+    final String? pcieAddress;
+
+    factory Worker.fromJson(Map<String, dynamic> json) => Worker(
+        index: json["Index"],
+        name: json["Name"],
+        power: json["Power"] == null ? null : json["Power"].toDouble(),
+        cclk: json["CCLK"],
+        mclk: json["MCLK"],
+        coreTemp: json["Core_Temp"],
+        jucTemp: json["Juc_Temp"],
+        memTemp: json["Mem_Temp"],
+        fanSpeed: json["Fan_Speed"],
+        lhrUnlockPct: json["LHR_Unlock_Pct"],
+        dualFactor: json["Dual_Factor"],
+        pcieAddress: json["PCIE_Address"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "Index": index,
+        "Name": name,
+        "Power": power,
+        "CCLK": cclk,
+        "MCLK": mclk,
+        "Core_Temp": coreTemp,
+        "Juc_Temp": jucTemp,
+        "Mem_Temp": memTemp,
+        "Fan_Speed": fanSpeed,
+        "LHR_Unlock_Pct": lhrUnlockPct,
+        "Dual_Factor": dualFactor,
+        "PCIE_Address": pcieAddress,
+    };
+}
---
+++ aisling/lib/widgets/analytics/mining_analytics.widget.dart
@@ -0,0 +1,175 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:aisling/models/lolminer.model.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class MiningAnalyWidget extends StatefulWidget {
+  const MiningAnalyWidget({Key? key}) : super(key: key);
+
+  @override
+  _MiningAnalyWidgetState createState() {
+    return _MiningAnalyWidgetState();
+  }
+}
+
+// @TODO modularize this
+class _MiningAnalyWidgetState extends State<MiningAnalyWidget> {
+  final StreamController<Lolminer> _streamCtrl = StreamController();
+
+
+  @override
+  void dispose() {
+    _streamCtrl.close();
+  }
+
+  @override
+  void initState() {
+
+
+    Timer mytimer = Timer.periodic(const Duration(seconds: 5), (timer) {
+      _fetchLolMiningData();
+    });
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<Lolminer>(
+      stream: _streamCtrl.stream,
+      builder: (context, snapshot) {
+        switch(snapshot.connectionState) {
+          case ConnectionState.waiting: return Center(child: Column(
+            children: [
+              Text(
+                'WAITING FOR MINING TO BEGIN - START MINING ABOVE TO UNLOCK',
+                style: Theme.of(context).textTheme.headline6
+              ),
+               SpinKitDancingSquare(
+                size: 100,
+                itemBuilder: (BuildContext context, int index) {
+                  return DecoratedBox(
+                    decoration: BoxDecoration(
+                      color: index.isEven ? const Color.fromARGB(255, 179, 179, 179) : const Color.fromARGB(255, 220, 220, 219),
+                    ),
+                  );
+                },
+              ),
+            ],
+          ));
+          default: if(snapshot.hasError){
+            return Text(
+              'Waiting for mining server...', style: Theme.of(context).textTheme.headline1);
+          } else {
+            return analyticsWidget(snapshot.data!);
+          }
+        }
+      }
+    );
+  }
+            // return Text(snapshot.data.software, style: Theme.of(context).textTheme.headline1);
+
+  Widget analyticsWidget(Lolminer _minerModel) {
+    return Column(
+      children: [
+        /* Make a col of workers inside an if state @TODO */
+
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text("Total Hash Rate:     ${_minerModel.algorithms![0].totalPerformance.toStringAsFixed(3)}Mh/s", style: Theme.of(context).textTheme.headline6),
+            Text("Accepted Shares:     ${_minerModel.algorithms![0].totalAccepted}", style: Theme.of(context).textTheme.headline6),
+            Text("Rejected Shares:     ${_minerModel.algorithms![0].totalRejected}", style: Theme.of(context).textTheme.headline6),
+            Text("Total Stales:     ${_minerModel.algorithms![0].totalStales}", style: Theme.of(context).textTheme.headline6),
+          ]
+        ),
+        const Divider(),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            for(var ijk in _minerModel.workers!) Text("Workers: ${ijk.name}", style: Theme.of(context).textTheme.bodySmall),
+            for(var ijk in _minerModel.workers!) Text("Power: ${ijk.power}W", style: Theme.of(context).textTheme.bodySmall),
+            for(var ijk in _minerModel.workers!) Text("LHR Unlock: ${ijk.lhrUnlockPct}%", style: Theme.of(context).textTheme.bodySmall),
+          ],
+        ),
+        const Divider(),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            for(var ijk in _minerModel.workers!) Text("CCLK: ${ijk.cclk}", style: Theme.of(context).textTheme.bodySmall),
+            for(var ijk in _minerModel.workers!) Text("MCLK: ${ijk.mclk}W", style: Theme.of(context).textTheme.bodySmall),
+            for(var ijk in _minerModel.workers!) Text("CORE_TEMP: ${ijk.coreTemp}%", style: Theme.of(context).textTheme.bodySmall),
+          ],
+        ),
+        const Divider(),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text("Software:     ${_minerModel.software}", style: Theme.of(context).textTheme.bodySmall),
+            Text("POOL:      ${_minerModel.algorithms![0].pool}s", style: Theme.of(context).textTheme.bodySmall),
+            Text("UPTIME:      ${_minerModel.session!.uptime}s", style: Theme.of(context).textTheme.bodySmall),
+            Text("# of workers: ${_minerModel.numWorkers}" , style: Theme.of(context).textTheme.bodySmall),
+            // Text("Workers:      ${_minerModel.workers}", style: Theme.of(context).textTheme.bodySmall)
+          ],
+        ),
+        const Divider(),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            // Text("Accepted Shares:     ${_minerModel.algorithms![0].totalAccepted}", style: Theme.of(context).textTheme.headlineSmall),
+            // Text("Rejected Shares:     ${_minerModel.algorithms![0].totalRejected}", style: Theme.of(context).textTheme.headlineSmall),
+            // Text("Total Hash Rate:     ${_minerModel.algorithms![0].totalPerformance}", style: Theme.of(context).textTheme.headlineSmall),
+            Text("Uptime:     ${_minerModel.session!.uptime}", style: Theme.of(context).textTheme.bodySmall),
+            Text("Wallet Mining Into:     ${_minerModel.algorithms![0].user}", style: Theme.of(context).textTheme.bodySmall),
+          ],
+        ),
+        const Divider(),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text("Algorithm:     ${_minerModel.algorithms![0].algorithm}", style: Theme.of(context).textTheme.bodySmall),
+            Text("Hash Rate:     ${_minerModel.algorithms![0].workerPerformance}Mh/s", style: Theme.of(context).textTheme.bodySmall),
+
+            Text("STARTUP:      ${_minerModel.session!.startup}", style: Theme.of(context).textTheme.bodySmall),
+            Text("last update: ${_minerModel.session!.lastUpdate}" , style: Theme.of(context).textTheme.bodySmall),
+            // Text("Workers:      ${_minerModel.workers}", style: Theme.of(context).textTheme.bodySmall)
+          ],
+        ),
+
+
+
+      ],
+    );
+  } 
+
+  /* @TODO set tto var of type in initState then run a timer and only call var names on type. Set type every time and just run functional code patterns instead of a stream */
+  Future<void> _fetchLolMiningData() async {
+    var _url =
+        Uri.parse('http://127.0.0.1:1339'); /* try this with 192 ip instead and then hook into aisling */
+    try {
+      var response = await http.get(_url);
+      print('Response status: ${response.statusCode}');
+
+      final miningData = Lolminer.fromJson(json.decode(response.body));
+
+      print(json.decode(response.body));
+
+      print(miningData);
+
+      _streamCtrl.sink.add(miningData);
+
+      // final parsedTrans = transactionsFromJson(_jsonBody);
+      print('#########     ${miningData.session?.uptime}    /   ${miningData.session?.lastUpdate} ####');
+      print('#########     ${miningData.software}    /    ${miningData.numWorkers} ##############');
+      print('Response status: ${response.statusCode}');
+
+      // return miningData;
+    } catch (e) {
+      print(e);
+    }
+  }
+}
+
